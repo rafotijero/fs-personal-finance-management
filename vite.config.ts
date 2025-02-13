41b6ecba67ab0a4d30896fe3cd35ac39adf7afe7
Rafael Tijero Fuentes <rtijero@contraloria.gob.pe>
2025-02-12 21:58:18 -0500

    [FRONTEND] Add new feature or fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Define el puerto dinámico de Render o usa 5173 en local
+const port = process.env.PORT ? parseInt(process.env.PORT) : 5173;
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: port, // Usa el puerto dinámico de Render o 5173 en local
+  },
+});
